4eb669c7d3cdd2600b99e69a91e73f7de2c199b6
sagacheng <sagacheng@tencent.com>
2022-08-09 07:52:16 +0800

    feat: update

---
+++ build.sh
@@ -0,0 +1,24 @@
+set -e
+
+cd build
+
+~/Documents/saga/emsdk/emsdk activate 2.0.21
+source ~/Documents/saga/emsdk/emsdk_env.sh
+
+build_and_move_lib() {
+  cmake -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DWASM_FEATURE=$wasm_feature ..
+  make -j8
+  mv yolov5-$wasm_feature.js yolov5-$wasm_feature.data yolov5-$wasm_feature.wasm ../public/
+}
+
+wasm_feature=basic
+build_and_move_lib
+
+wasm_feature=simd
+build_and_move_lib
+
+wasm_feature=threads
+build_and_move_lib
+
+wasm_feature=simd-threads
+build_and_move_lib
