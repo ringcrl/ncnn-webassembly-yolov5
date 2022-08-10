11ec03c49290cc54b9346b5d9fd4d5c725836ade
ringcrl <ringcrl@foxmail.com>
2022-08-10 23:11:25 +0800

    feat: update

--- main.js
+++ main.js
@@ -1,179 +1,176 @@
-var Module = {
-  locateFile(filename) {
-    return './public/' + filename;
+const Module = {
+  locateFile (filename) {
+    return './public/' + filename
   }
-};
+}
 
-var has_simd;
-var has_threads;
+let hasSimd
+let hasThreads
+let yolov5ModuleName
 
-var wasmModuleLoaded = false
-var wasmModuleLoadedCallbacks = [];
+let wasmModuleLoaded = false
+const wasmModuleLoadedCallbacks = []
 
 Module.onRuntimeInitialized = function () {
-  wasmModuleLoaded = true;
-  for (var i = 0; i < wasmModuleLoadedCallbacks.length; i++) {
-    wasmModuleLoadedCallbacks[i]();
+  wasmModuleLoaded = true
+  for (let i = 0; i < wasmModuleLoadedCallbacks.length; i++) {
+    wasmModuleLoadedCallbacks[i]()
   }
 }
 
 wasmFeatureDetect.simd().then(simdSupported => {
-  has_simd = simdSupported;
+  hasSimd = simdSupported
 
   wasmFeatureDetect.threads().then(threadsSupported => {
-    has_threads = threadsSupported;
+    hasThreads = threadsSupported
 
-    if (has_simd) {
-      if (has_threads) {
-        yolov5_module_name = 'yolov5-simd-threads';
+    if (hasSimd) {
+      if (hasThreads) {
+        yolov5ModuleName = 'yolov5-simd-threads'
+      } else {
+        yolov5ModuleName = 'yolov5-simd'
       }
-      else {
-        yolov5_module_name = 'yolov5-simd';
-      }
-    }
-    else {
-      if (has_threads) {
-        yolov5_module_name = 'yolov5-threads';
-      }
-      else {
-        yolov5_module_name = 'yolov5-basic';
+    } else {
+      if (hasThreads) {
+        yolov5ModuleName = 'yolov5-threads'
+      } else {
+        yolov5ModuleName = 'yolov5-basic'
       }
     }
 
-    console.log('load ' + yolov5_module_name);
+    console.log('load ' + yolov5ModuleName)
 
-    var yolov5wasm = 'public/' + yolov5_module_name + '.wasm';
-    var yolov5js = 'public/' + yolov5_module_name + '.js';
+    const yolov5wasm = 'public/' + yolov5ModuleName + '.wasm'
+    const yolov5js = 'public/' + yolov5ModuleName + '.js'
 
     fetch(yolov5wasm)
       .then(response => response.arrayBuffer())
       .then(buffer => {
-        Module.wasmBinary = buffer;
-        var script = document.createElement('script');
-        script.src = yolov5js;
+        Module.wasmBinary = buffer
+        const script = document.createElement('script')
+        script.src = yolov5js
         script.onload = function () {
-          console.log('Emscripten boilerplate loaded.');
+          console.log('Emscripten boilerplate loaded.')
         }
-        document.body.appendChild(script);
-      });
+        document.body.appendChild(script)
+      })
+  })
+})
 
-  });
-});
+let shouldFaceUser = true
+let stream = null
+const w = 640
+let h = 480
 
-var shouldFaceUser = true;
-var stream = null;
-var w = 640;
-var h = 480;
-
-var dst = null;
-var resultarray = null;
-var resultbuffer = null;
+let dst = null
+let video
+let canvas
+let ctx
+let switchcamerabtn
 window.addEventListener('DOMContentLoaded', function () {
-  var isStreaming = false;
-  switchcamerabtn = document.getElementById('switch-camera-btn');
-  video = document.getElementById('video');
-  canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
+  let isStreaming = false
+  switchcamerabtn = document.getElementById('switch-camera-btn')
+  video = document.getElementById('video')
+  canvas = document.getElementById('canvas')
+  ctx = canvas.getContext('2d')
 
   // Wait until the video stream canvas play
   video.addEventListener('canplay', function (e) {
     if (!isStreaming) {
       // videoWidth isn't always set correctly in all browsers
-      if (video.videoWidth > 0) h = video.videoHeight / (video.videoWidth / w);
-      canvas.setAttribute('width', w);
-      canvas.setAttribute('height', h);
-      isStreaming = true;
+      if (video.videoWidth > 0) h = video.videoHeight / (video.videoWidth / w)
+      canvas.setAttribute('width', w)
+      canvas.setAttribute('height', h)
+      isStreaming = true
     }
-  }, false);
+  }, false)
 
   // Wait for the video to start to play
   video.addEventListener('play', function () {
-    //Setup image memory
-    var id = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    var d = id.data;
+    // Setup image memory
+    const id = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const d = id.data
 
     if (wasmModuleLoaded) {
-      mallocAndCallSFilter();
+      mallocAndCallSFilter()
     } else {
-      wasmModuleLoadedCallbacks.push(mallocAndCallSFilter);
+      wasmModuleLoadedCallbacks.push(mallocAndCallSFilter)
     }
 
-    function mallocAndCallSFilter() {
+    function mallocAndCallSFilter () {
       if (dst != null) {
-        _free(dst);
-        dst = null;
+        _free(dst)
+        dst = null
       }
 
-      dst = _malloc(d.length);
+      dst = _malloc(d.length)
 
-      //console.log("What " + d.length);
+      // console.log("What " + d.length);
 
-      sFilter();
+      sFilter()
     }
-  });
+  })
 
   // check whether we can use facingMode
-  var supports = navigator.mediaDevices.getSupportedConstraints();
-  if (supports['facingMode'] === true) {
-    switchcamerabtn.disabled = false;
+  const supports = navigator.mediaDevices.getSupportedConstraints()
+  if (supports.facingMode === true) {
+    switchcamerabtn.disabled = false
   }
 
   switchcamerabtn.addEventListener('click', function () {
-    if (stream == null)
-      return
+    if (stream == null) { return }
 
     stream.getTracks().forEach(t => {
-      t.stop();
-    });
+      t.stop()
+    })
 
-    shouldFaceUser = !shouldFaceUser;
-    capture();
-  });
+    shouldFaceUser = !shouldFaceUser
+    capture()
+  })
 
-  capture();
-});
+  capture()
+})
 
-function capture() {
-  var constraints = { audio: false, video: { width: 640, height: 480, facingMode: shouldFaceUser ? 'user' : 'environment' } };
+function capture () {
+  const constraints = { audio: false, video: { width: 640, height: 480, facingMode: shouldFaceUser ? 'user' : 'environment' } }
   navigator.mediaDevices.getUserMedia(constraints)
     .then(function (mediaStream) {
-      var video = document.querySelector('video');
-      stream = mediaStream;
-      video.srcObject = mediaStream;
+      const video = document.querySelector('video')
+      stream = mediaStream
+      video.srcObject = mediaStream
       video.onloadedmetadata = function (e) {
-        video.play();
-      };
+        video.play()
+      }
     })
     .catch(function (err) {
-      console.log(err.message);
-    });
+      console.log(err.message)
+    })
 }
 
+function ncnnYolov5 () {
+  const canvas = document.getElementById('canvas')
+  const ctx = canvas.getContext('2d')
 
-function ncnn_yolov5() {
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  const data = imageData.data
 
-  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  var data = imageData.data;
+  HEAPU8.set(data, dst)
 
-  HEAPU8.set(data, dst);
+  _yolov5_ncnn(dst, canvas.width, canvas.height)
 
-  _yolov5_ncnn(dst, canvas.width, canvas.height);
-
-  var result = HEAPU8.subarray(dst, dst + data.length);
-  imageData.data.set(result);
-  ctx.putImageData(imageData, 0, 0);
+  const result = HEAPU8.subarray(dst, dst + data.length)
+  imageData.data.set(result)
+  ctx.putImageData(imageData, 0, 0)
 }
 
-//Request Animation Frame function
-var sFilter = function () {
-  if (video.paused || video.ended) return;
+// Request Animation Frame function
+function sFilter () {
+  if (video.paused || video.ended) return
 
-  ctx.fillRect(0, 0, w, h);
-  ctx.drawImage(video, 0, 0, w, h);
+  ctx.fillRect(0, 0, w, h)
+  ctx.drawImage(video, 0, 0, w, h)
 
-  ncnn_yolov5();
+  ncnnYolov5()
 
-  window.requestAnimationFrame(sFilter);
+  window.requestAnimationFrame(sFilter)
 }
